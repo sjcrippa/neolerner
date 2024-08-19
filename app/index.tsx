9b2fb1f297b3c4f0acfaf96bebb8354d11ed7c39
Santiago Crippa <santicrippa@gmail.com>
2024-08-19 18:14:03 -0300

    Adding supabase instance

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,29 @@
+import 'react-native-url-polyfill/auto'
 import { Text, View } from "react-native";
-import Button from "../src/auth/components/Button";
+import { useEffect, useState } from "react";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "@/src/shared/lib/supabase";
+import Auth from "@/src/auth/components/Auth";
 
 export default function Index() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     <View className="flex-1 justify-center items-center bg-black ">
-      <Text className="text-2xl text-white">Welcome</Text>
-      <Button />
+      <View>
+        <Auth />
+        {session && session.user && <Text>{session.user.id}</Text>}
+      </View>
     </View>
   )
 }
